d212a771a5251d989ae9b54989b0fea11e0a14b0
dreamcareer7 <dreamcareer070@gmail.com>
2019-12-17 14:47:44 -0300

    Updating compiled files

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -4,6 +4,6 @@ import { IConfig } from "./config";
 declare type Optional<T> = {
     [P in keyof T]?: (T[P] | undefined);
 };
-declare function ExpressPeerServer(server: Server, options?: IConfig): import("express-serve-static-core").Express;
+declare function ExpressPeerServer(server: Server, options?: Optional<IConfig>): import("express-serve-static-core").Express;
 declare function PeerServer(options?: Optional<IConfig>, callback?: (server: Server) => void): import("express-serve-static-core").Express;
 export { ExpressPeerServer, PeerServer };
